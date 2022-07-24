ff178977266e33629a2e1c9c5632b987d8a63990
Olha <vasylieva.hell@gmail.com>
2022-07-24 19:09:48 +0300

    homework5

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,18 @@
-from django.db import models # noqa
+from django.db import models
 
-# Create your models here.
+
+class Rate(models.Model):
+    base_currency_type = models.CharField(max_length=5)
+    currency_type = models.CharField(max_length=5)
+    sale = models.DecimalField(max_digits=8, decimal_places=3)
+    buy = models.DecimalField(max_digits=8, decimal_places=3)
+    source = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField()
+    email_to = models.EmailField()
+    subject = models.CharField(max_length=64)
+    message = models.CharField(max_length=500)
+
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import greeting
+from currency.views import greeting, rate_list, contactus
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('greeting/', greeting),
+    path('rate/list/', rate_list),
+    path('contactus/list/', contactus),
 ]
